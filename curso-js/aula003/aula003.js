dee56693a6e590bfc0bce93282511f46edf26bc6
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-21 19:53:52 -0300

    update curso-js/aula003/aula003.js

--- curso-js/aula003/aula003.js
+++ curso-js/aula003/aula003.js
@@ -3,8 +3,18 @@
 //Uma variavel é um espaço na memoria na sua maquina que guarda o conteudo dentro dela.
 //E essa variavel eu posso reutilizar e mudar o conteudo guardado dentro dela.
 
-if (true) {
+/* if (true) {
     var nome = "escopo de variaveis";
 }
 
-console.log(nome);
+console.log(nome); */
+
+function escopo(){
+    let nome = "Hestia";
+    if (true) {
+        console.log("Dentro do escopo do if " + nome);
+    }
+    console.log("Fora do escopo do if" + nome);
+}
+
+console.log("Fora do escopo " + nome);

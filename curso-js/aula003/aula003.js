607aa33c531ef57dfb7dbf34e23017c7e91012cb
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-21 19:32:18 -0300

    update curso-js/aula003/aula003.js

--- curso-js/aula003/aula003.js
+++ curso-js/aula003/aula003.js
@@ -1,3 +1,4 @@
 //diferença entre var, let e const
 
 //Uma variavel é um espaço na memoria na sua maquina que guarda o conteudo dentro dela.
+//E essa variavel eu posso reutilizar e mudar o conteudo guardado dentro dela.

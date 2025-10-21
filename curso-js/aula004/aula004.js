3161773760d8fa90001a4b7ef93966ae7bff0c1f
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-21 20:40:34 -0300

    update curso-js/aula004/aula004.js

--- curso-js/aula004/aula004.js
+++ curso-js/aula004/aula004.js
@@ -1,2 +1,15 @@
 //Aula sobre operadores matematico
-let num1 = 2 
+
+//Declarando variavel em linhas separadas.
+/* let num1 = 1;
+let num2 = 1;
+
+console.log(num1, num2); */
+
+//==================
+
+//Declarando variaveis na mesma linha;
+
+let num1 = 12, num2 = 12; 
+console.log(num1);
+console.log(num2);

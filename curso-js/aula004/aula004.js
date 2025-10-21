9788fdc2f93ae3a83fcabd4ed894a4c6ccf62c7d
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-21 20:43:06 -0300

    update curso-js/aula004/aula004.js

--- curso-js/aula004/aula004.js
+++ curso-js/aula004/aula004.js
@@ -10,6 +10,15 @@ console.log(num1, num2); */
 
 //Declarando variaveis na mesma linha;
 
-let num1 = 12, num2 = 12; 
+// let num1 = 12, num2 = 12; 
+// console.log(num1);
+// console.log(num2);
+
+
+//Terceira forma de declarar variaveis
+
+let num1, num2, num3;
+num1, num2, num3 = 12;
 console.log(num1);
 console.log(num2);
+console.log(num3);

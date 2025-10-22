7c11b3dfffdeb7fb8b5886821e2507cb4a424fe7
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-22 16:33:31 -0300

    update curso-js/aula010/aula010.js

--- curso-js/aula010/aula010.js
+++ curso-js/aula010/aula010.js
@@ -1,11 +1,18 @@
 //Aula sobre operadores ternario
 
-let n = 10;
-let u = 20;
+let num1 = 10;
+res = (!(num1 % 2) ? "par" : "impa");
+console.log(res)
 
-if (n > u) {
-    console.log("sim, é")
+
+/* if (!(num1 === 0)) {
+    console.log("par");
 }else{
-    console.log("não, não é")
-}
+    console.log("impa")
+} */
 
+
+let n1 = 10
+let n2 = 2 
+
+res = (n1 > n2 ? "sim, n1 é maior que n2" : "não, n1 não é maior que n2");

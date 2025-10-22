6d446bf7eb8d72a7b8e0e1638d60e13db10e6cd0
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-22 14:15:07 -0300

    update curso-js/aula009/aula009.js

--- curso-js/aula009/aula009.js
+++ curso-js/aula009/aula009.js
@@ -3,6 +3,7 @@ let n = 10;
 
 n++
 //Quando colocamos o operador ++ na direta do valor
-//eu estou incrementando um valor, nese caso, 1
+//eu estou incrementando um valor, nese caso, n++ rebece 1
+//como incremento. assim o "n que valia 10, passa a valor 11"
 
 console.log(n)

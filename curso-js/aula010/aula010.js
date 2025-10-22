34529140af2e1262c885986ee84849dd6d3bdb0b
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-22 17:40:55 -0300

    update curso-js/aula010/aula010.js

--- curso-js/aula010/aula010.js
+++ curso-js/aula010/aula010.js
@@ -6,7 +6,7 @@
 
 let num1 = 10;
 res = (!(num1 % 2) ? "par" : "impar");
-console.log(res)
+console.log(res);
 
 //Teste logico do valor ternario ? se true e : se false
 // 0 = false 
@@ -22,14 +22,14 @@ console.log(res2)
 } */
 
 
-let n1 = 10
-let n2 = 20 
+let n1 = 10;
+let n2 = 20;
 
 res3 = (n1 > n2 ? "sim, n1 é maior que n2" : "não, n1 não é maior que n2");
 console.log(res3)
 
-let stts = prompt("Digite A ou I").toLocaleLowerCase().trim()
+let stts = prompt("Digite A ou I").toLocaleLowerCase().trim();
 
 res4 = (stts === "a" ? "Ativo" : "Inativo");
 
-console.log(res4)
+console.log(res4);

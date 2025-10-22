3bd2eed7c9c12ee193e3bc28fcde2b501cd645da
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-22 14:17:46 -0300

    update curso-js/aula009/aula009.js

--- curso-js/aula009/aula009.js
+++ curso-js/aula009/aula009.js
@@ -1,9 +1,10 @@
 // aula de pré e pós incremento
 let n = 10;
 
-n++
+n++;
+console.log(n);
 //Quando colocamos o operador ++ na direta do valor
 //eu estou incrementando um valor, nese caso, n++ rebece 1
 //como incremento. assim o "n que valia 10, passa a valor 11"
 
-console.log(n)
+console.log(n++);

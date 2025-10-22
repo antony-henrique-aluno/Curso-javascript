8e2a315f232491210f8cfcfef347676c6bc230fe
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-22 17:36:14 -0300

    update curso-js/aula010/aula010.js

--- curso-js/aula010/aula010.js
+++ curso-js/aula010/aula010.js
@@ -27,3 +27,7 @@ let n2 = 20
 
 res3 = (n1 > n2 ? "sim, n1 é maior que n2" : "não, n1 não é maior que n2");
 console.log(res3)
+
+let stts = "a".toLocaleLowerCase();
+
+res = (stts === "a" ? "Ativo" : "Inanito");

c0581649df4e29e181de4439f8d64923f402f724
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-21 23:29:49 -0300

    update curso-js/aula005/aula005.js

--- curso-js/aula005/aula005.js
+++ curso-js/aula005/aula005.js
@@ -1,1 +1,18 @@
 //Aula sobre operadores relacionais
+
+/* > maior 
+>= maior ou igual
+< menor
+<= menor ou igual
+!= diferente
+== igual */
+
+//=============testes===========//
+let num1 = 10
+let num2 = 5 
+let num3 = 10
+
+console.log(num1 < num2) //false
+console.log(num2 > num1) //false
+
+console.log(num1 != num3)

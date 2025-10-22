9290a9cdaaa99fd43e12d65bab1d5d595c8efb59
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-22 00:57:23 -0300

    update curso-js/aula007/aula007.js

--- curso-js/aula007/aula007.js
+++ curso-js/aula007/aula007.js
@@ -4,3 +4,15 @@
 // ! -> not 
 
 //O valor dos resultados das operações logicas sera verdadeiro ou falso
+
+//TABELA TRUE / VERDADEIRO 
+// and --> &&
+
+let n1, n2, n3, n4;
+
+n1 = 10;
+n2 = 5;
+n3 = 15;
+n4 = 2;
+
+console.log((n1 > n2))

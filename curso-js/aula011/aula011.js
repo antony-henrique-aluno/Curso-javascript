be221038c15a54f2e7e38de5767d0de50914748a
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 15:43:53 -0300

    update curso-js/aula011/aula011.js

--- curso-js/aula011/aula011.js
+++ curso-js/aula011/aula011.js
@@ -1,10 +1,12 @@
 //Aula sobre typeof 
-let v1 = 10;
+let v1 = 10.4;
 let v2 = "10";
 let v3 = v1 === v2;
 let v4 = {nome: "Ghislane"};
 
-console.log("Valor: " + v1 + " Tipo: " + typeof(v1));
-console.log("Valor: " + v2 + " Tipo: " + typeof(v2));
-console.log("Valor: " + v3 + " Tipo: " + typeof(v3));
-console.log("Valor: " + v4 + " Tipo: " + typeof(v4));
+//O operador typeof retorna o nome do tipo do valor
+
+console.log("Valor: " + v1 + " Tipo: " + typeof(v1)); //Tipo number
+console.log("Valor: " + v2 + " Tipo: " + typeof(v2)); //Tipo string
+console.log("Valor: " + v3 + " Tipo: " + typeof(v3)); //Tipo boolean
+console.log("Valor: " + v4 + " Tipo: " + typeof(v4)); //Tipo objeto, uma array

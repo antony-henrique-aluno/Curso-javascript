5105687285242ba249d84e393ad295e7cd160454
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 01:18:15 -0300

    update curso-js/aula007/exercicio/ex.js

--- curso-js/aula007/exercicio/ex.js
+++ curso-js/aula007/exercicio/ex.js
@@ -14,11 +14,13 @@ if (speed < 65) {
 }else if (speed < 75) {
     console.log("uma boa velocidade, ainda no limite")
 }else{
-    console.log("se ferror")
+    console.log("se ferror");
 }
 
-console.log("---------")
+console.log("---------");
 
 //Uma logica qualquer 2
 let casaVenda = 150000;
 let valorEntrada = prompt("Digite o valor de entrada");
+let anosPagar = prompt("Em quantos anos você vai pagar");
+let valorTotal = casaVenda - valorEntrada;

dc2cebd91781d25ad8df16c0b02e6bb7e8a36a21
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 20:51:10 -0300

    update curso-js/aula014/aula014.js

--- curso-js/aula014/aula014.js
+++ curso-js/aula014/aula014.js
@@ -3,7 +3,7 @@
 let colocacao = 4;
 switch (colocacao) {
     case 1:
-       console.log("Caso colocacao seja 1, retorna essa msg!") 
+       console.log("Caso colocacao seja 1, retorna essa msg!");
        //A cada final de um case,preciso colocar um break;
         break;
     case 2:

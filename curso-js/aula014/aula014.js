6f9184057c4cb521505e0df7f10a63dd3889f8b1
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 20:43:32 -0300

    update curso-js/aula014/aula014.js

--- curso-js/aula014/aula014.js
+++ curso-js/aula014/aula014.js
@@ -1,1 +1,14 @@
 //Aula sobre switch case 
+
+let colocacao = 2;
+switch (colocacao) {
+    case 1:
+       console.log("Caso colocacao seja 1, retorna essa msg!") 
+       //A cada final de um case,preciso colocar um break;
+        break;
+    case 2:
+        console.log("Segundo lugar");
+        break;
+    default:
+        
+}

c5c8e979d67a058d4a43c3421f6167a908c5be47
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 15:56:38 -0300

    update curso-js/aula012/aula012.js

--- curso-js/aula012/aula012.js
+++ curso-js/aula012/aula012.js
@@ -2,6 +2,8 @@
 
 let n1 = [10, 12,14];
 let n2 = [20, 24,28, 30, 32, 34];
-let n3 = [10, 12,14];
+let n3 
 
-console.log(n1)
+console.log("n1: " + n1)
+console.log("n2: " + n2)
+console.log("n3: " + n3)

9c4319550e87cf6faca5243c181b723c7010fb14
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 16:24:21 -0300

    update curso-js/aula012/aula012.js

--- curso-js/aula012/aula012.js
+++ curso-js/aula012/aula012.js
@@ -11,3 +11,6 @@
 
 const jogador1 = {nome: "Ghislane", energia: 100, vidas: 3};
 const jogador2 = {nome: "Eris", energia: 150, vidas: 28};
+const jogador3 = {...jogador1, ...jogador2};
+
+console.log(jogador3)

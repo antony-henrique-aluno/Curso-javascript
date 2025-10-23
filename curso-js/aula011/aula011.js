11c6b1602cb806c35c65ab5adca3d1cfad7eb934
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 15:33:16 -0300

    update curso-js/aula011/aula011.js

--- curso-js/aula011/aula011.js
+++ curso-js/aula011/aula011.js
@@ -5,6 +5,6 @@ let v3 = v1 === v2;
 let v4 = {nome: "Ghislane"};
 
 console.log(typeof(v1));
-console.log(v2);
-console.log(v3);
-console.log(v4);
+console.log(typeof(v2));
+console.log(typeof(v3));
+console.log(typeof(v4));

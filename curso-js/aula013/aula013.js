d38d4fd2ae0f568712e4e2c39e027ababd3e91ef
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 18:49:09 -0300

    update curso-js/aula013/aula013.js

--- curso-js/aula013/aula013.js
+++ curso-js/aula013/aula013.js
@@ -2,7 +2,7 @@
 
 let num1 = 11;
 if (num1 > 10) {
-    console.log("Sim, num1 é maior que 10")
+    console.log("Sim, num1 é maior que 10");
 }else{
-    console.log("Não, não é maior")
+    console.log("Não, não é maior");
 }

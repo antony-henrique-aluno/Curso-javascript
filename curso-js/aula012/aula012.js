d2be54609cff990d83f641cf72c0c3ae9c475e44
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 16:18:58 -0300

    update curso-js/aula012/aula012.js

--- curso-js/aula012/aula012.js
+++ curso-js/aula012/aula012.js
@@ -2,8 +2,9 @@
 
 let n1 = [10, 12,14];
 let n2 = [20, 24,28, 30, 32, 34];
-let n3 = [n1, n2];
+let n3 = [...n1, ...n2];
 
 // console.log("n1: " + n1)
 // console.log("n2: " + n2)
 console.log("n3: " + n3)
+console.log("Tipo: " + typeof(n3))

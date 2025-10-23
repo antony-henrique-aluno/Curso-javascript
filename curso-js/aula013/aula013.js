ee5b27fa7f2283773e5bb78f1fb5562235cb425a
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 18:51:50 -0300

    update curso-js/aula013/aula013.js

--- curso-js/aula013/aula013.js
+++ curso-js/aula013/aula013.js
@@ -1,9 +1,13 @@
 //Aula sobre estrutura condicionais
 
-let num1 = 11;
+/* let num1 = 11;
 if (num1 > 10) {
     console.log("Sim, num1 é maior que 10");
 }else{
     console.log("Não, não é maior");
 }
+ */
+
+
+//Usando logica
 

fb4313c032c19c7b6b1ff7dbd84d1e2a7ddb36bd
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 01:06:08 -0300

    update curso-js/aula007/exercicio/ex.js

--- curso-js/aula007/exercicio/ex.js
+++ curso-js/aula007/exercicio/ex.js
@@ -7,3 +7,16 @@ rel=(speed > 90 ? "Você está alêm do limite permitido" : "Você está no valo
 
 console.log(rel) */
 
+let speed = 64;
+
+if (speed < 65) {
+    console.log("Uma boa velocidade limite");
+}else if (speed < 75) {
+    console.log("uma boa velocidade, ainda no limite")
+}else{
+    console.log("se ferror")
+}
+
+console.log("---------")
+
+//Uma logica qualquer 2

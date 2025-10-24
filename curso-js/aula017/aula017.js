95cff35e25c51d3b79ed360352c701338c59c082
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 20:50:49 -0300

    update curso-js/aula017/aula017.js

--- curso-js/aula017/aula017.js
+++ curso-js/aula017/aula017.js
@@ -1,1 +1,13 @@
-//Aula sobre o loop for
+//Aula sobre o loop while 
+
+let cont = 0 
+while(cont < 10){
+    console.log(cont)
+    cont++
+}
+
+
+let nome = "hestia";
+while(nome !== "hestia"){
+    console.log("tenta dnovo cara")
+}

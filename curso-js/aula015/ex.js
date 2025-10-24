f39ef545b75afe21289ac36d8cc9970aae783c50
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 10:00:08 -0300

    update curso-js/aula015/ex.js

--- curso-js/aula015/ex.js
+++ curso-js/aula015/ex.js
@@ -29,5 +29,5 @@
 // Dado o array, imprima cada elemento
 const frutas = ['maçã', 'banana', 'laranja', 'uva'];
 for (let i = 0; i < frutas.length; i++) {
-    
+    console.log(`Frutas ${frutas[i]}: e seu index ${i}`)
 }

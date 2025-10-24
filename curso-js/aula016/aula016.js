1e9c1a5773c03755bbe9ef83246e5e0237ac3df7
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 19:25:51 -0300

    update curso-js/aula016/aula016.js

--- curso-js/aula016/aula016.js
+++ curso-js/aula016/aula016.js
@@ -1,15 +1,20 @@
 //Aula sobre for in e for of
 
 
+const arr = [10 ,11, 30, 31, 40, 41]
+
+
+for(posi in arr){
+    console.log(posi)
+}
+
 
 // percorrendo uma array com o for tradicional
-
-const arr = [10 ,11, 30, 31, 40, 41]
-for(let i = 0; i < arr.length; i++){
+/* for(let i = 0; i < arr.length; i++){
     // let soma = 2 * arr[i];
     // console.log(soma)
     console.log(i); //Imprimindo a posição de cada elemento desse array
     console.log("------------")
     console.log(arr[i]); //Imprimindo cada elemento dentro do array
 }
-
+ */

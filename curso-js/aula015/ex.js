8fd2f7bd0b871f34e6338a0855ff40cad4c3ae31
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 09:13:35 -0300

    update curso-js/aula015/ex.js

--- curso-js/aula015/ex.js
+++ curso-js/aula015/ex.js
@@ -7,4 +7,7 @@ for(let i = 1; i <= 10; i++){
 //Imprimindo apenas numeros pares 
 for(let i = 0; i <= 20; i++){
     let res = (i % 2 === 0 ? `${i}` : `${i}`)
+    console.log(res)
 }
+
+//// Calcule a soma de todos os números de 1 a 100

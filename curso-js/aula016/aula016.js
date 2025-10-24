f9937fb00fcc99bce5b36d07cce4c40c593f2383
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 19:17:08 -0300

    update curso-js/aula016/aula016.js

--- curso-js/aula016/aula016.js
+++ curso-js/aula016/aula016.js
@@ -6,6 +6,8 @@
 
 const arr = [10 ,11, 30, 31, 40, 41]
 for(let i = 0; i < arr.length; i++){
-    let soma = 2 * arr[i];
-    console.log(soma)
+    // let soma = 2 * arr[i];
+    // console.log(soma)
+    
+    console.log(i) //Imprimindo cada elemento desse array
 }

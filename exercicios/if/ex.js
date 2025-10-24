bda408df1047f7be225910037f7f9a28c081d366
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 10:30:39 -0300

    update exercicios/if/ex.js

---
+++ exercicios/if/ex.js
@@ -0,0 +1,7 @@
+// Verifique se uma pessoa é maior de idade (18 anos ou mais)
+const idade = 17;
+if(idade >= 18){
+    console.log("É maior de idade")
+}else{
+    console.log("Tem menos que " + idade + " anos")
+}

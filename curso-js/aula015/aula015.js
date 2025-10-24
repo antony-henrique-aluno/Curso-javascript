c264405ef15424792784039ed1b9c5a43d92d235
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-23 22:15:03 -0300

    update curso-js/aula015/aula015.js

--- curso-js/aula015/aula015.js
+++ curso-js/aula015/aula015.js
@@ -1,1 +1,10 @@
 //Aula sobre o loop for
+
+// O loop for tem a seguinte sintax e para faze-lo funcionar
+// eu preciso de 3 areas dentro do loop for!
+
+for (let primeiro a inicialização = 
+0; segundo a condição de execução < 
+limit; e terceiro, o controle++) {
+    
+}

e3e9ee7999b6c89880a83a178ffe86377e6945e8
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 09:21:31 -0300

    update curso-js/aula015/ex.js

--- curso-js/aula015/ex.js
+++ curso-js/aula015/ex.js
@@ -1,16 +1,16 @@
 
 //Contagem regressiva de 1 a 10;
-for(let i = 1; i <= 10; i++){
-    console.log(`${i}`);
-}
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${i}`);
+// }
 
 //Imprimindo apenas numeros pares 
-for(let i = 0; i <= 20; i++){
+/* for(let i = 0; i <= 20; i++){
     let res = (i % 2 === 0 ? `${i}` : `${i}`)
     console.log(res)
-}
+} */
 
-//// Calcule a soma de todos os números de 1 a 100
+//Calcule a soma de todos os números de 1 a 100
 for(let i = 0; i <= 100; i++){
     
 }

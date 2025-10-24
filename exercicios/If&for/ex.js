3bf6febf5bb1ed29be1d53f2d1c410486d7f04f9
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 14:43:12 -0300

    update exercicios/If&for/ex.js

--- exercicios/If&for/ex.js
+++ exercicios/If&for/ex.js
@@ -1,4 +1,6 @@
 // Encontre todos os números entre 1 e 50 que são divisíveis por 3
 for(let i = 1; i <= 50; i++){
-    
+    if (i % 3 === 0) {
+        console.log(`Todos os numeros impar são ${i}`)
+    }
 }

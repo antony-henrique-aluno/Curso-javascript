76355af856aa10b7d913d5af0992a774e9099d40
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 19:19:04 -0300

    update curso-js/aula016/aula016.js

--- curso-js/aula016/aula016.js
+++ curso-js/aula016/aula016.js
@@ -8,6 +8,6 @@ const arr = [10 ,11, 30, 31, 40, 41]
 for(let i = 0; i < arr.length; i++){
     // let soma = 2 * arr[i];
     // console.log(soma)
-    console.log(i); //Imprimindo cada elemento desse array
+    console.log(i); //Imprimindo a posição de cada elemento desse array
 }
 

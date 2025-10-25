b2ae6978ab3e8ce0827a0efdfb3d8ba4618da916
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-25 00:49:48 -0300

    update curso-js/aula019/break&continue.js

--- curso-js/aula019/break&continue.js
+++ curso-js/aula019/break&continue.js
@@ -3,3 +3,7 @@
 
 //O break é uma expressão que vai gerar uma interrupção
 //na execução
+
+//O continue é um pouco diferente! O continue quando 
+//executado dentro de um loop ele para somente aquela
+//interação e pula para a proxima interação

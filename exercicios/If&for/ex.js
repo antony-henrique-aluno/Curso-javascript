49ec38c2a1e9d97e8d366994658c1164411343fa
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-25 18:16:25 -0300

    update exercicios/If&for/ex.js

--- exercicios/If&for/ex.js
+++ exercicios/If&for/ex.js
@@ -1,24 +1,4 @@
-// Encontre todos os números entre 1 e 50 que são divisíveis por 3
-for(let i = 1; i <= 50; i++){
-    if (i % 3 === 0) {
-        console.log(`Todos os numeros impar são ${i}`)
-    }
-}
 
+//Encontre o maior numero de uma array 
 
-// Conte quantas vogais existem em uma string
-const texto = "";
-let cont = 0 
-let vogais = "aeiou";
-for(let i = 0; i < texto.length; i++){
-    if(vogais.indexOf(texto[i]) !== -1){
-        cont++
-        console.log(cont)
-    }
-}
-
-// Encontre o maior número em um array
-const numeros = [3, 7, 2, 9, 1, 4];
-for(let i = 0; i < numeros.length; i++){
-    
-}
+let arr = [10, 15, 20, 25, 30];

6e6720ba8f9dfe01e4e8eb06451bae46ecf72751
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-25 00:56:23 -0300

    update curso-js/aula019/break&continue.js

--- curso-js/aula019/break&continue.js
+++ curso-js/aula019/break&continue.js
@@ -6,4 +6,15 @@
 
 //O continue é um pouco diferente! O continue quando 
 //executado dentro de um loop ele para somente aquela
-//interação e pula para a proxima interação
+//interação e pula para a proxima interação e continua
+//normalmente a execução, ele só não vai executar 
+//a interação atual.
+
+
+//Exemplos
+let n = 0;
+let max = 100;
+while(n < max){
+    console.log(`Loopando ${n}`);
+    n++
+}

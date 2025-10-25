e717c50e34a460c437a13b83dfe067b886aac39d
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-25 00:24:45 -0300

    update curso-js/aula018/doWhile.js

--- curso-js/aula018/doWhile.js
+++ curso-js/aula018/doWhile.js
@@ -1,1 +1,18 @@
 //Aula sobre do while
+
+
+
+//A diferença entre while e  do while é que, o do while
+//vai ao menos garantir a execução do programa ao menos
+//uma vez
+let n = 0 
+while(n < 10){
+    console.log(n)
+    n++
+}
+
+let n = 0 
+do{
+    console.log(n)
+    n++
+}while(n < 10)

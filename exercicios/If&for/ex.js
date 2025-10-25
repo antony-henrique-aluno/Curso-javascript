b22e7c9ce9d3d7ea6c69582ef20f1b904eade367
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-25 18:39:32 -0300

    update exercicios/If&for/ex.js

--- exercicios/If&for/ex.js
+++ exercicios/If&for/ex.js
@@ -1,13 +1,16 @@
 
 //Encontre o maior numero de uma array 
 
-let arr = [10, 15, 20, 25, 30];
-let maiorNum = arr[0] //Começa com o maior numero de uma array 
+let arr = [10, 15, 20, 25, 30, 1000];
+let maiorNum = arr[0]; //Começa com o maior numero de uma array 
 
+//Pecorre cada elemento da array
 for (let i = 0; i < arr.length; i++) {
+    //Compara o elemento atual com o maior elemento
+    //encontrado dentro da array
     if (arr[i] > maiorNum) {
-        maiorNum = arr[i]
+        maiorNum = arr[i]; //Atualiza a array com o maior numero encontrado
     }
     // console.log(maiorNum)
 }
-console.log(maiorNum)
+console.log(maiorNum); //Imprime o valor

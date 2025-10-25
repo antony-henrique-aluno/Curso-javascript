2493bc225a8541399fd66ddf3f70e387f98a5f9a
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-25 16:15:32 -0300

    update exercicios/if/ex.js

--- exercicios/if/ex.js
+++ exercicios/if/ex.js
@@ -19,4 +19,8 @@ if (idade >= 18 && idade < 24) {
     console.log("Essa garota já é maior de idade e tambem uma bela milf");
 }else if (idade >= 32 && idade < 42) {
     console.log("Essa garota já é maior de idade, mas continua linda! desconfio que é uma elfa");
+}else if(idade >= 42){
+    console.log("Com toda a certeza é uma elfa");
+}else{
+    console.log("Ainda é menor de idade")
 }

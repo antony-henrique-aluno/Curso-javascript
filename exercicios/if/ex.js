23896cf7852da3bf4ef1b9cd256b01be6cea2e58
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-25 16:07:41 -0300

    update exercicios/if/ex.js

--- exercicios/if/ex.js
+++ exercicios/if/ex.js
@@ -1,53 +1,11 @@
-// Verifique se uma pessoa é maior de idade (18 anos ou mais)
-const idade = 17;
-if(idade >= 18){
-    console.log("É maior de idade")
+
+
+
+
+// 1-Verificando se um numero é positivo ou negativo
+let n = 5 
+if (n < 0) {
+    console.log(`${n} é um numero negativo`)
 }else{
-    console.log("Tem menos que " + idade + " anos")
-}
-
-// Classifique uma nota: A (>=90), B (>=80), C (>=70), D (<70)
-const nota = 80;
-
-if (nota >= 90) {
-    console.log("Sua nota é A")
-}else if (nota >= 80) {
-    console.log("Sua nota é B")
-}else if (nota >= 70) {
-    console.log("Sua nota é C")
-}else{
-    console.log("Sua nota é D")
-}
-
-
-// Verifique se um dia é fim de semana
-const dia = 'domingo';
-if (dia === "segunda") {
-    console.log("Hoje é segunda")
-}else if (dia === "terça") {
-    console.log("Hoje é terça")
-}else if (dia === "quarta") {
-    console.log("Hoje é quarta")
-}else if (dia === "quinta") {
-    console.log("Hoje é quinta")
-}else if("Hoje é sexa"){
-    console.log("Hoje é sexta");
-}else if(dia === "sabado" || dia === "domingo"){
-    console.log("Hoje é " + dia + " dia de semana");
-}
-
-if (dia === "sábado" || dia === "domingo") {
-    console.log(`Hoje é ${dia}! Um dia de semana`);
-}else{
-    console.log(`Hoje é ${dia}!`);
-}
-
-// Verifique se um número é positivo, negativo ou zero
-const numero = -5;
-if (numero < 0) {
-    console.log("É numero negativo")
-}else if (numero > 0) {
-    console.log("É numero positivo")
-}else{
-    console.log("É numero é zero")
+    console.log(`${n} é um numero positivo`)
 }

3da70825cc6f003f50eea3640baa3a02efe728b4
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-24 22:01:11 -0300

    update curso-js/aula017/aula017.js

--- curso-js/aula017/aula017.js
+++ curso-js/aula017/aula017.js
@@ -8,6 +8,13 @@ while(cont < 10){
 
 
 //Fatorial de 5! 5*4*3*2*1 
+//O loop while é otimo quando precisamos
+//repetir programas quando não sabemos quando vai acabar
 
 let n = 5 
 let fatorial = 1;
+while (n >= 1) {
+    fatorial *= n
+    n--
+}
+console.log(fatorial)

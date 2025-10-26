af7f9ad41201e30b99da6ca31033e326ac084d03
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-26 14:58:15 -0300

    update exercicios/adivinhacao/ex.js

--- exercicios/adivinhacao/ex.js
+++ exercicios/adivinhacao/ex.js
@@ -2,19 +2,18 @@
 // Dica: use prompt() para entrada do usuário
 
 let cpu = Math.floor(Math.random() * 10) + 1;
-let usuario = 4
-let tentativas = 3;
 let win = false
 
-for (let i = 0; i < tentativas; i++) {
+for (let tentativas = 3; tentativas > 0; tentativas--) {
+    let usuario = Number(prompt("Tente adivinhar o numero da cpu"))
     if (usuario === cpu) {
         console.log(`Seu numero foi ${usuario} o da cpu foi ${cpu}`)
         win = true;
         break;
     }else{
-        console.log(`Você errou, seu numero foi ${usuario} o da cpu foi ${cpu}`);
+        console.log(`Você errou, seu numero foi ${usuario} o da cpu foi ${cpu}. Você sô tem ${tentativas} restante!`);
     }
-    if (!win && tentativas === 2) {
-        console.log(`${tentativas} você não tem mais tentativas`)
-    }
+}
+if (!win) {
+    console.log("Suas tentativas não existem mais")
 }

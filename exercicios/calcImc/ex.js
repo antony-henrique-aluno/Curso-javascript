2d243f61b117dd954be45e7e6c596515b675646c
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-26 04:14:13 -0300

    update exercicios/calcImc/ex.js

--- exercicios/calcImc/ex.js
+++ exercicios/calcImc/ex.js
@@ -4,5 +4,9 @@ let altura = Number(prompt("Digite a sua altura"));
 let resultado = peso / (altura * 2);
 
 if (peso === null || peso === 0 || altura === null || altura === 0) {
-    console.log("Digite novamente")
+    console.log("Digite novamente");
+}  
+if (resultado < 18) {
+    console.log("Abaixo do peso!");
+    
 }

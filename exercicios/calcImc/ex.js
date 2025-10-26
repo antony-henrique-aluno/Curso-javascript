824f0adf3d8d25c9ae1ccfea3aa795539549db85
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-26 04:34:32 -0300

    update exercicios/calcImc/ex.js

--- exercicios/calcImc/ex.js
+++ exercicios/calcImc/ex.js
@@ -2,11 +2,19 @@
 let peso = Number(prompt("Digite o seu peso"));
 let altura = Number(prompt("Digite a sua altura"));
 let resultado = peso / (altura * 2);
+let imcValue = resultado.toFixed(2);
+switch (peso === null || altura === null) {
+    case 1:
+        console.log(`O prompt ${peso} e o prompt ${altura} estão vazias, digite algo.`)
+        break;
+}
 
-if (peso === null || peso === 0 || altura === null || altura === 0) {
-    console.log("Digite novamente");
-}  
-if (resultado < 18) {
-    console.log("Abaixo do peso!");
-    
+if (imcValue < 18) {
+    console.log("Abaixo do peso! " + imcValue);
+}else if(resultado < 25){
+    console.log("Peso normal " + imcValue)
+}else if (resultado < 30) {
+    console.log("Está ficando obeso " + imcValue)
+}else{
+    console.log("Está obeso " + imcValue)
 }

b8addd8c9625cc70fcde6efddcb724a30e1b581c
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-26 04:10:40 -0300

    update exercicios/calcImc/ex.js

--- exercicios/calcImc/ex.js
+++ exercicios/calcImc/ex.js
@@ -1,4 +1,8 @@
 //Calculando o imc
-let peso = prompt("Digite o seu peso");
+let peso = Number(prompt("Digite o seu peso"));
 let altura = Number(prompt("Digite a sua altura"));
+let resultado = peso / (altura * 2);
 
+if (peso === null || peso === 0 || altura === null || altura === 0) {
+    console.log("Digite novamente")
+}

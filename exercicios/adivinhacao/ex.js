d7e1ad9f90bc09c7993c125622b59e8b9b222e1b
antony-henrique-aluno <meugithub220@gmail.com>
2025-10-26 14:22:36 -0300

    update exercicios/adivinhacao/ex.js

--- exercicios/adivinhacao/ex.js
+++ exercicios/adivinhacao/ex.js
@@ -1,4 +1,6 @@
 // Crie um loop que permita ao usuário tentar adivinhar o número
 // Dica: use prompt() para entrada do usuário
 
-let cpu = math.floor(math.random() * 10) + 1;
+let cpu = Math.floor(Math.random() * 10) + 1;
+let usuario = Number(prompt("Tente adivinha o numero da CPU"));
+let tentativas = 3;
